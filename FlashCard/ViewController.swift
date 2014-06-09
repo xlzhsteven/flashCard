39374f853df0001b967f6fc39eaee85e85168489
Zhang, Xiaolong <xiaolozhang@paypal.com>
2014-06-08 22:39:41 -0700

    Initial Commit

--- FlashCard/ViewController.swift
+++ FlashCard/ViewController.swift
@@ -7,8 +7,44 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    @IBOutlet var question:UITextField!
+    @IBOutlet var answer:UITextField!
+    @IBOutlet var display:UITextView!
+    
+    @IBAction func btnsave(){
+        var appDel:AppDelegate=UIApplication.sharedApplication().delegate as AppDelegate
+        var context:NSManagedObjectContext = appDel.managedObjectContext
+        
+        var cardInfo = NSEntityDescription.insertNewObjectForEntityForName("Card", inManagedObjectContext: context) as NSManagedObject
+        cardInfo.setValue(""+question.text, forKey: "question")
+        cardInfo.setValue(""+answer.text, forKey: "answer")
+        context.save(nil)
+        question.text=""
+        answer.text=""
+    }
+    @IBAction func btnLoad(){
+        var appDel:AppDelegate=UIApplication.sharedApplication().delegate as AppDelegate
+        var context:NSManagedObjectContext = appDel.managedObjectContext
+        
+        var request = NSFetchRequest(entityName: "Card")
+        request.returnsObjectsAsFaults=false
+        request.predicate = NSPredicate(format: "question = %@", ""+question.text)
+        var results:NSArray = context.executeFetchRequest(request, error: nil)
+        if results.count>0 {
+            var res = results[0] as NSManagedObject
+            question.text = res.valueForKey("question") as String
+            display.text = res.valueForKey("answer") as String
+        } else {
+            println("something went wrong")
+        }
+        
+    }
+    
+    
+    
                             
     override func viewDidLoad() {
         super.viewDidLoad()
